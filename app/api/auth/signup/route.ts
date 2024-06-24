84e32ee653748f682376e7dd22883645ba2a55f4
Ini Atoyebi <iatoyebi.3134@gmail.com>
2024-06-24 16:25:47 +0100

    sign up function completed

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,8 +1,10 @@
 import '@/app/utility/zod-extensions';
 import { NextRequest } from 'next/server';
+import { cookies } from 'next/headers';
 import { signupSchema } from './validation';
 import bcrypt from 'bcrypt';
 import Database from '@/app/db';
+import { lucia } from '@/app/auth/setup';
 
 export async function POST(req: NextRequest) {
 	const saltRounds = 10;
@@ -17,20 +19,51 @@ export async function POST(req: NextRequest) {
 
 		const { username, email, password, firstname, lastname } = data!;
 
+		//query the user table in the database via email
+		const db = await (await Database.getInstance()).getClient();
+		const findUserQuery = {
+			text: 'SELECT * FROM "user" WHERE email = $1',
+			values: [email]
+		};
+
+		const existingFoundUserCount =
+			(await db.query(findUserQuery))?.rowCount || 1;
+
+		if (existingFoundUserCount > 0) {
+			return Response.json(
+				{ success: false, message: 'Error! User already exists' },
+				{ status: 400 }
+			);
+		}
+
 		const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-		const db = (await Database.getInstance()).getClient();
-		const query = {
+		const createUserInsert = {
 			text: 'INSERT INTO "user"(firstname, lastname, email, username, password) VALUES($1, $2, $3, $4, $5) RETURNING *',
 			values: [firstname, lastname, email, username, hashedPassword]
 		};
 
-		(await db).query(query);
+		const user = (await db.query(createUserInsert)).rows[0];
+
+		delete user.password;
+		delete user.created_at;
+
+		const session = await lucia.createSession(user.id, {});
+
+		const sessionCookie = await lucia.createSessionCookie(session.id);
+		cookies().set(
+			sessionCookie.name,
+			sessionCookie.value,
+			sessionCookie.attributes
+		);
+
+		await db.end();
 
 		return Response.json(
 			{
 				success: true,
-				message: 'User has been successfully signed up'
+				message: `User ${firstname} ${lastname} has been Successfully Signed Up!`,
+				data: user
 			},
 			{ status: 200 }
 		);
